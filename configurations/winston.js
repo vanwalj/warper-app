439b04c62a8d381055e6ed8475ec545c54866252
Jordan Van Walleghem <j.vanwall@gmail.com>
2015-02-21 23:30:00 +0000

    Herokouille

--- configurations/winston.js
+++ configurations/winston.js
@@ -1,3 +1,16 @@
 /**
  * Created by Jordan on 2/20/2015.
  */
+"use strict";
+
+var winston = require('winston'),
+    Slack = require('winston-slack-pls').Slack;
+
+winston.setLevels(winston.config.npm.levels);
+
+winston.add(Slack, {
+    apiToken: process.env.SLACK_API,
+    channel: process.env.SLACK_WINSTON_CHANNEL
+});
+
+module.exports = winston;
