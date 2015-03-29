dd0ce74262af77636c3d462a8fdd369130c198a4
Jordan <j.vanwall@gmail.com>
2015-03-29 02:16:34 +0100

    Lourd refactor.

--- index.js
+++ index.js
@@ -4,11 +4,36 @@
 
 var restify     = require('restify'),
     winston     = require('winston'),
-    passport    = require('passport'),
     server      = restify.createServer({
         name: "Warper !",
         version: "1.0.0"
     });
+
+server.on('InternalServerError', function (req, res, err, cb) {
+    winston.error(err);
+    return cb();
+});
+
+server.on('after', function (req, res, route, error) {
+    winston.log('info', "HTTP LOG", { request: {
+        time: req.time(),
+        headers: {
+            contentLength: req.contentLength(),
+            contentType: req.contentType(),
+            href: req.href(),
+            path: req.path()
+        },
+        secured: req.isSecure(),
+        chunked: req.isChunked(),
+        keepAlive: req.isKeepAlive(),
+        params: req.params,
+        body: req.body
+    }, response: {
+        statusCode: res.statusCode,
+        send: res._data
+    }, route: route,
+        error: error });
+});
 
 server.use(restify.CORS());
 server.use(restify.bodyParser({
