8de159696f706d2ff0ea3a70c875381a1328e380
Jordan Van Walleghem <j.vanwall@gmail.com>
2015-04-04 02:25:29 +0100

    SQS.

--- worker/index.js
+++ worker/index.js
@@ -5,20 +5,14 @@
 
 var Consumer = require('sqs-consumer');
 
-var timers = require('timers');
-
 var app = Consumer.create({
     queueUrl: process.env.AWS_SQS_S3_UPLOAD,
     region: process.env.AWS_REGION,
     handleMessage: function (message, done) {
-        console.log(message);
         try {
             var body = JSON.parse(message.Body);
-            console.log('Receive message', body);
-            timers.setTimeout(function () {
-                console.log('Done msg');
-                done();
-            }, 10000);
+            console.log(body);
+            done();
         }
         catch (e) {
             console.log(e)
@@ -30,4 +24,4 @@ app.on('error', function (err) {
     console.log('ERR', err.message);
 });
 
-app.start();
+if (process.env.NODE_ENV == 'test') app.start();
